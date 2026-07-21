8945872d3d22b80991b1d791a39d9bb26cb3fa78
rdrapeau <3769522+rdrapeau@users.noreply.github.com>
2026-07-20 21:13:53 -0700

    Configure 1password_native_host.sh wrapper for AMO signing version 4.7.5.102

---
+++ 1password_native_host.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+exec "/Applications/1Password.app/Contents/Library/LoginItems/2BUA8C4S2C.com.agilebits.onepassword-osx-helper.app/Contents/MacOS/OnePasswordNativeMessageHost" "onepassword4@agilebits.com"
